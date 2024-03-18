d6637a5f9a7db5a4169868a2620dfb7043d21fa1
Kashishpatel09 <136715275+Kashishpatel09@users.noreply.github.com>
2024-03-18 12:50:04 +0530

    Add files via upload

---
+++ MUI/Mui_components.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+function Mui_components() {
+  return (
+    <div>
+   <Helmet>
+    <link rel="pingback" href="https://themepanthers.com/wp/nest/d1/xmlrpc.php"/>
+    <link rel='stylesheet' id='bootstrap-css' href='https://themepanthers.com/wp/nest/d1/wp-content/plugins/nest-addons/assets/css/vendors/bootstrap.min.css?ver=5.0.2' type='text/css' media='all' />
+    <link rel='stylesheet' id='elementor-frontend-css' href='https://themepanthers.com/wp/nest/d1/wp-content/uploads/elementor/css/custom-frontend-lite.min.css?ver=1690513242' type='text/css' media='all' />
+    <link rel='stylesheet' id='nest-main-style-css' href='https://themepanthers.com/wp/nest/d1/wp-content/plugins/nest-addons/assets/css/sass/main.css?ver=1710740716' type='text/css' media='all' />
+    <link rel='stylesheet' id='nest-scondary-style-css' href='https://themepanthers.com/wp/nest/d1/wp-content/themes/steelthemes-nest/assets/css/overwrite/theme-extra.css?ver=6.4.3' type='text/css' media='all' />
+    <link rel='stylesheet' id='nest-main-mb-style-css' href='https://themepanthers.com/wp/nest/d1/wp-content/themes/steelthemes-nest/assets/css/overwrite/mobile.css?ver=6.4.3' type='text/css' media='all' />
+   </Helmet>
+  <div>
+    <header className="header-area header-style-1  header-height-2">
+      <div className="auto-container">
+        <div className="row align-items-center">
+          <div className="col-xl-4 col-lg-4 col-md-12">
+            <div className="header-info top_left">
+              <ul>
+                <li><a href target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">About Us</a></li>
+                <li><a href target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">My Account </a></li>
+                <li><a href target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">Wishlist </a></li>
+                <li><a href target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">Order Tracking</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-xl-4 col-lg-4 col-md-12">
+            <div className="text-center">
+              <div className="swiper  news-flash">
+                <div className="swiper-wrapper">
+                  <div className="swiper-slide">
+                    <a href target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">100% Secure delivery without contacting the courier</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header></div>
+  <div className="header-middle header-middle-ptb-1">
+    <div className="auto-container">
+      <div className="header-wrap">
+        <div className="logo logo-width-1">
+          <a href="https://themepanthers.com/wp/nest/d1" className="logo navbar_brand">
+            <img decoding="async" src="https://themepanthers.com/wp/nest/d1/wp-content/uploads/2022/02/logo.png" alt="Nest" className="logo_default" />
+          </a>
+        </div>
+        <div className="header-right">
+          <div className="search-style-2">
+            <div className="dgwt-wcas-search-wrapp dgwt-wcas-is-detail-box dgwt-wcas-has-submit woocommerce dgwt-wcas-style-solaris js-dgwt-wcas-layout-classic dgwt-wcas-layout-classic js-dgwt-wcas-mobile-overlay-enabled">
+              <form className="dgwt-wcas-search-form" role="search" action="https://themepanthers.com/wp/nest/d1/" method="get">
+                <div className="dgwt-wcas-sf-wrapp">
+                  <label className="screen-reader-text" htmlFor="dgwt-wcas-search-input-1">Products search</label>
+                  <input id="dgwt-wcas-search-input-1" type="search" className="dgwt-wcas-search-input" name="s" defaultValue placeholder="Search for products..." autoComplete="off" />
+                  <div className="dgwt-wcas-preloader" />
+                  <div className="dgwt-wcas-voice-search" />
+                  <button type="submit" aria-label="Search" className="dgwt-wcas-search-submit">Search</button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="header-action-right">
+            <div className="header-action-2">
+              <div className="button_text">
+                <a href="https://themepanthers.com/wp/nest/d1/contact/" className="btn btn-sm" target="&quot;_blank&quot;" rel="&quot;nofollow&quot;">Your Location<i className="fi-rs-arrow-right" /></a>
+              </div>
+              <div className="button_text">
+                <p>Compare</p>
+              </div>
+              <div className="button_text">
+                <p>Wishlist</p>
+              </div>
+              <div className="button_text">
+                <p>Cart</p>
+              </div>
+              <div className="button_text">
+                <p>Account</p>
+              </div>
+            </div>
+          </div>
+          <section>
+            <div className="container">
+              <div className="d-flex"> 
+                <button style={{marginLeft: 90, borderRadius: 5, backgroundColor: 'rgb(99, 205, 99)', border: 'rgb(99, 205, 99)'}}>
+                  Browser ALL Categories
+                </button>
+                <ul className="d-flex" style={{marginLeft: 90}}>
+                  <p style={{marginLeft: 30}}>Home</p>
+                  <p style={{marginLeft: 30}}>About</p>
+                  <p style={{marginLeft: 30}}>Shop</p>
+                  <p style={{marginLeft: 30}}>Blog</p>
+                  <p style={{marginLeft: 30}}>Contact</p>
+                  <p style={{marginLeft: 30}}>Mega menu</p>
+                  <p style={{marginLeft: 30}}>Viewser</p>
+                </ul>
+              </div>
+            </div>
+          </section>
+          <div className="container-fluid" style={{padding: 20}}>
+            <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img className="d-block w-100" src="https://previews.123rf.com/images/floralset/floralset1511/floralset151100949/47609638-vector-illustration-flat-header-shopping-web-store-global-communication-trading-e-business-commerce.jpg" alt="First slide" />
+                </div>
+                <div className="carousel-item">
+                  <img className="d-block w-100" src="https://img.freepik.com/free-vector/online-shopping-horizontal-banner-illustration_1284-57252.jpg" alt="Second slide" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div></div></div></div>
+
+  )
+}
+
+export default Mui_components
